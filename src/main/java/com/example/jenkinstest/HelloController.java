5ab6fe83c0f3cc23318e20d27e232ca69fdc2b86
DoHungCuong <cuong.do@gmail.com>
2022-10-05 20:48:32 +0700

    Add hello page

--- src/main/java/com/example/jenkinstest/HelloController.java
+++ src/main/java/com/example/jenkinstest/HelloController.java
@@ -1,8 +1,16 @@
 package com.example.jenkinstest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
     HelloController() {
         String s = "3";
         String st2 = "4";
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 }
